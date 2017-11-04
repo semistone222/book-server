888ba53c51d5124b104908eac6381f1e9d8169c4
semistone222 <semistone222@gmail.com>
2017-11-05 01:31:19 +0900

    changed web to thymeleaf

--- src/main/java/kr/or/connect/bookserver/presentation/MainController.java
+++ src/main/java/kr/or/connect/bookserver/presentation/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.or.connect.bookserver.domain.ShortUrl;
 import kr.or.connect.bookserver.service.ShortUrlService;
@@ -23,17 +24,17 @@ public class MainController {
 	}
 	
 	@GetMapping("/")
-	String index(Model model) {
-		model.addAttribute("name", "semistone222");
-		return "index.html";
+	String index(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "index";
 	}
 	
 	// TODO : error
 //	@GetMapping("/{shortenedUrl}")
-//	void redirect(@PathVariable String shortenedUrl) {
+//	void redirect(@PathVariable("shortenedUrl") String shortenedUrl) {
 //		ShortUrl shortUrl = service.findByShortenedUrl(shortenedUrl);
 //		String originalUrl = shortUrl.getOriginalUrl();
-//		// TODO : redirect to original url when requested using shortened url
+//		 TODO : redirect to original url when requested using shortened url
 //		log.info("redirect to : {}" + shortUrl);
 //	}
 
